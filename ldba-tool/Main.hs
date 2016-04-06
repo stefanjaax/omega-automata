730872da4999879526696127d4f5359bdd4895ab
Stefan Jaax <stefan.jaax@googlemail.com>
2016-04-06 11:14:10 +0200

    Adding argument -isldba
    Add option -isldba to ldba-tool
    (returns true iff. input automaton is limit-deterministic).

--- ldba-tool/Main.hs
+++ ldba-tool/Main.hs
@@ -51,7 +51,15 @@ actionUnion s1 s2 = do
   putStrLn $ toHoa $ nbaToHoa (buchiUnion nba1 nba2)
 
 
+actionIsLDBA :: String -> IO ()
+actionIsLDBA s = do
+  hoa <- parseHoaOrExit (pack s)
+  let isLDBA = isLimitDeterministic $ hoaToNBA hoa
+  putStrLn (if isLDBA then "true" else "false")
+
+
 actionsForArgs :: [String] -> IO ()
+actionsForArgs ("-isldba":xs) = pipeOrFile xs actionIsLDBA
 actionsForArgs ("-2ldba":xs) = pipeOrFile xs action2LDBA
 actionsForArgs ("-complement":xs) = pipeOrFile xs action2Complement
 actionsForArgs ("-intersection":xs) = pipeOrFiles xs actionIntersection
@@ -84,6 +92,7 @@ usage = do
   putStrLn "ldba-tool - A tool for limit-deterministic Buchi automata. \n"
   putStrLn "Usage:  ldba-tool [args]\n"
   putStrLn "args:"
+  putStrLn " -isldba [fname]                  Tell if NBA is limit-deterministic"
   putStrLn " -2ldba [fname]                   Convert NBA to LDBA"
   putStrLn " -complement [fname]              Complement NBA to LDBA"
   putStrLn " -intersection fname [fname]      Intersection-LDBA of two LDBAs"
