a98c40489be6f29c9ad15d883575754e97a48b60
Stefan Jaax <stefan.jaax@googlemail.com>
2016-04-06 12:24:10 +0200

    Documentation
    * Reformat code
    * Add haddock comments to source files

--- src/OmegaAutomata/Automata.hs
+++ src/OmegaAutomata/Automata.hs
@@ -8,43 +8,65 @@ import Data.List (groupBy, nub)
 
 type State = Int
 
-data NBA q a l = NBA{ states :: S.Set q
-                    , bimap :: B.Bimap q Node
-                    , graph :: Gr l a
-                    , start :: S.Set q
-                    , accept :: S.Set q
+-- | Data-type for non-deterministic Buchi automata
+data NBA q a l = NBA{ states :: S.Set q       -- ^ The states of the NBA
+                    , bimap :: B.Bimap q Node -- ^ Bijection between states and nodes in the graph
+                    , graph :: Gr l a         -- ^ The internal graph representing transitions
+                    , start :: S.Set q        -- ^ The initial states in the NBA
+                    , accept :: S.Set q       -- ^ The set of accepting states in the NBA
                     } deriving (Show)
 
+
+-- | Returns node in internal graph corresponding to state
 toNode :: (Ord q) => NBA q a l -> q -> Node
 toNode a q = (bimap a) B.! q
 
 
+-- | Returns state corresponding to node in internal graph
 toState :: (Ord q) => NBA q a l -> Node -> q
 toState a q = (bimap a) B.!> q
+   -- ^ The intersection-NBA
 
 
-succs :: (Ord q) => NBA q a l -> q -> [(q, a)]
+-- | Returns successors of state with corresponding edge-labels
+succs :: (Ord q) => NBA q a l   -- ^ The NBA the state belongs to
+                 -> q           -- ^ The state
+                 -> [(q, a)]    -- ^ Successor states and corresponding edge-label.
 succs a q = (\(q, l) -> (toState a q, l)) <$> lsuc (graph a) (toNode a q)
 
 
-pres :: (Ord q) => NBA q a l -> q -> [(q, a)]
+-- | Returns predecessors of state with corresponding edge-labels
+pres :: (Ord q) => NBA q a l    -- ^ The NBA the state belongs to
+                -> q            -- ^ The state
+                -> [(q, a)]     -- ^ Predecessors and corresponding edge-label.
 pres a q = (\(q, l) -> (toState a q, l)) <$> lpre (graph a) (toNode a q)
 
 
-aSuccs :: (Ord q) => NBA q a l -> q -> a -> [q]
+-- | Returns list of successor-states for a given state and edge-label
+aSuccs :: (Ord q) => NBA q a l   -- ^ The NBA the state belongs to
+                  -> q           -- ^ The state
+                  -> a           -- ^ The edge-label
+                  -> [q]         -- ^ List of successors for state and edge-label.
 aSuccs a q b = [toState a q' | (q', b) <- lsuc (graph a) (toNode a q)]
 
 
-aPres :: (Ord q) => NBA q a l -> q -> a -> [q]
+-- | Returns list of predecessor-states for a given state and edge-label
+aPres :: (Ord q) => NBA q a l   -- ^ The NBA the state belongs to
+                 -> q           -- ^ The state
+                 -> a           -- ^ The edge-label
+                 -> [q]         -- ^ List of predecessors for state and edge-label.
 aPres a q b = [toState a q' | (q', b) <- lpre (graph a) (toNode a q)]
 
 
-trans :: (Ord q) => NBA q a l -> [(q, a, q)]
+-- | Returns all labelled transitions defined in a given NBA
+trans :: (Ord q) => NBA q a l     -- ^ The NBA
+                 -> [(q, a, q)]   -- ^ List of labelled transitions defined in NBA
 trans a = [(q1, l, q2) | (i1, i2, l) <- labEdges (graph a)
                        , let q1 = (bimap a) B.!> i1
                        , let q2 = (bimap a) B.!> i2]
 
 
+-- | Collects states with same label in a list
 combine :: (Eq a, Ord q) => [(q, a)] -> [([q], a)]
 combine xs = let xs' = groupBy (\x y -> snd x == snd y) xs in
   case xs' of
@@ -52,14 +74,20 @@ combine xs = let xs' = groupBy (\x y -> snd x == snd y) xs in
     _ -> map (\ys -> (fst <$> ys, head (snd <$> ys))) xs'
 
 
+-- | Lifts successor-relation of a state to successor-relation of lists of states
 powerSucc :: (Eq a, Ord q) => NBA q a l -> [q] -> [([q], a)]
 powerSucc a qs = map (\(xs, l) -> (nub xs, l)) $ combine $ concat [succs a q | q <- qs]
 
 
+-- | Same as 'powerSucc', only for distinct symbol
 powerASucc :: (Ord a, Ord q) => NBA q a l -> [q] -> a -> [q]
 powerASucc a qs l = concat [aSuccs a q l | q <- qs]
 
-annotateStates :: (Ord q, Ord i) => i -> NBA q a l -> NBA (i, q) a l
+
+-- | Annotates states of an automaton
+annotateStates :: (Ord q, Ord i) => i                 -- ^ The value used for annotating
+                                 -> NBA q a l         -- ^ The original NBA
+                                 -> NBA (i, q) a l    -- ^ The annotated NBA
 annotateStates i a = let annotate = S.map (\x -> (i, x)) in
                       NBA{ states = annotate (states a)
                          , bimap = B.fromList [((i, q), n) | (q, n) <- (B.assocs (bimap a))]
@@ -69,7 +97,10 @@ annotateStates i a = let annotate = S.map (\x -> (i, x)) in
                          }
 
 
-insertStates :: (Ord q) => [(q, l)] -> NBA q a l -> NBA q a l
+-- | Inserts states into an NBA
+insertStates :: (Ord q) => [(q, l)]     -- ^ List of labelled states to be inserted
+                        -> NBA q a l    -- ^ The original NBA
+                        -> NBA q a l    -- ^ The NBA resulting from inserting states
 insertStates qls a = let qs = fst <$> qls
                          ls = snd <$> qls
                          newNs = newNodes (length qs) (graph a)
@@ -82,7 +113,9 @@ insertStates qls a = let qs = fst <$> qls
                              }
 
 
-alphabet :: (Eq a) => NBA q a l -> [a]
+-- | Returns list of edge-labels defined in an NBA
+alphabet :: (Eq a) => NBA q a l   -- ^ The NBA
+                   -> [a]         -- ^ List of edge-labels defined in NBA
 alphabet a =  nub $ edgeLabel <$> labEdges (graph a)
 
 
@@ -90,21 +123,31 @@ graphNodes :: NBA q a l -> [Node]
 graphNodes a = nodes (graph a)
 
 
-label :: (Ord q) => q -> NBA q a l -> l
+-- | Returns label of a state in an NBA
+label :: (Ord q) => q           -- The state
+                 -> NBA q a l   -- The NBA the state is contained in
+                 -> l           -- The label of the state
 label q a = let l = lab (graph a) (bimap a B.! q) in
   case l of
     (Just x) -> x
-    Nothing -> error "Node has no label."
+    Nothing -> error "Node has no label." -- This should never happen.
 
 
-insertTrans :: (Ord q) => [(q, a, q)] -> NBA q a l -> NBA q a l
+-- | Inserts transitions into NBA.
+insertTrans :: (Ord q) => [(q, a, q)]  -- ^ List of transitions
+                       -> NBA q a l    -- ^ The original NBA
+                       -> NBA q a l    -- ^ The NBA resulting from inserting the transitions
 insertTrans ts a = let edges = [(i1, i2, l) | (q1, l, q2) <- ts
                                             , let i1 = bimap a B.! q1
                                             , let i2 = bimap a B.! q2] in
                         a{graph = insEdges edges (graph a)}
 
 
-buchiUnion :: (Ord q) => NBA q a l -> NBA q a l -> NBA (Int, q) a l
+-- | Returns union of two Buchi automata.
+-- | If the automata are limit-deterministic, the so is the returned automaton.
+buchiUnion :: (Ord q) => NBA q a l                -- ^ The first NBA
+                      -> NBA q a l                -- ^ The second NBA
+                      -> NBA (Int, q) a l         -- ^ The union-NBA
 buchiUnion a1 a2 = let (a1', a2') = (annotateStates 1 a1, annotateStates 2 a2)
                        qs2' = [(q, label q a2') | q <- S.toList (states a2')]
                        trans2' = trans a2'
@@ -113,7 +156,11 @@ buchiUnion a1 a2 = let (a1', a2') = (annotateStates 1 a1, annotateStates 2 a2)
                          , accept = S.union (accept a1') (accept a2')}
 
 
-buchiIntersection :: (Ord q) => NBA q a l -> NBA q a l -> NBA (Int, (q, q)) a (l, l)
+-- | Returns Intersection of two Buchi automata.
+--   If the automata are limit-deterministic, then so is the returned automaton.
+buchiIntersection :: (Ord q) => NBA q a l                    -- ^ The first NBA
+                             -> NBA q a l                    -- ^ The second NBA
+                             -> NBA (Int, (q, q)) a (l, l)   -- ^ The intersection-NBA
 buchiIntersection a1 a2 = let stateList = S.toList . states
                               newStates = [(q1, q2) | q1 <- (stateList a1), q2 <- (stateList a2)]
                               newTrans = [((q1, q2), l, (q1', q2')) | (q1, l, q1') <- trans a1
@@ -138,12 +185,19 @@ buchiIntersection a1 a2 = let stateList = S.toList . states
                                                                                  , accept = newAcc
                                                                                  }
 
-
-buchiComplement :: (Ord q) => NBA q a l -> NBA q a l
+-- | Returns Complement-automaton of a Buchi automaton.
+-- Resulting automaton is always limit-deterministic.
+buchiComplement :: (Ord q) => NBA q a l   -- ^ The NBA to be complemented
+                           -> NBA q a l   -- ^ The complement-NBA
 buchiComplement a = undefined
 
 
-makeNBA :: (Ord q) => [(q, l)] -> [(q, a, q)] -> [q] -> [q] -> NBA q a l
+-- | Constructs an NBA from components
+makeNBA :: (Ord q) => [(q, l)]           -- ^ List of labelled states
+                   -> [(q, a, q)]        -- ^ List of labelled transitions
+                   -> [q]                -- ^ List of start-states
+                   -> [q]                -- ^ List of accepting states
+                   -> NBA q a l          -- ^ The resulting NBA
 makeNBA qs ts ss as = let lNodes = zip [1..] (snd <$> qs)
                           assoc = zip (fst <$> qs) [1..] in
                            insertTrans ts $ NBA{ states = S.fromList [q | (q, l) <- qs]
--- src/OmegaAutomata/LDBA.hs
+++ src/OmegaAutomata/LDBA.hs
@@ -1,5 +1,5 @@
 -- | Manipulation of Limit-deterministic Buchi automata
-module OmegaAutomata.LDBA where
+module OmegaAutomata.LDBA(isLimitDeterministic, toLDBA) where
 import OmegaAutomata.Automata
 import Data.Graph.Inductive
 import Data.Graph.Inductive.Query.TransClos (trc)
@@ -10,7 +10,10 @@ import Data.List (nub)
 
 type PowerPair q = ([q], [q])
 
-isLimitDeterministic :: (Ord q) => NBA q a l -> Bool
+
+-- | Check whether NBA is limit-deterministic
+isLimitDeterministic :: (Ord q) => NBA q a l  -- ^ The NBA to be checked
+                                   -> Bool    -- ^ Bool-value indicating whether automaton is limit-det.
 isLimitDeterministic a = let g = graph a
                              fs = accept a
                              ls = [q2 | (q1, q2, _) <- (labEdges . trc) g, S.member (toState a q1) fs]
@@ -18,6 +21,23 @@ isLimitDeterministic a = let g = graph a
                               all detSucc ls
 
 
+-- | Convert NBA into equivalent LDBA using Courcoubetis and Yannakakis' construction
+--   (c.f. article "The Complexity of Probabilistic Verification" by the same authors).
+toLDBA :: (Ord q, Ord a) =>  NBA q a l           -- ^ The NBA to be converted
+          -> NBA (Int, PowerPair q) a ([l], [l]) -- ^ The equivalent LDBA
+toLDBA a = let ts = pairTransClosure a [([f],[f]) | f <- S.toList (accept a)]
+               ps = concat [[p1, p2] | (p1, a, p2) <- ts]
+               ls = [(labels q1 a, labels q2 a) | (q1, q2) <- ps]
+               as = [(qs1, qs2) | (qs1, qs2) <- ps, qs1 == qs2]
+               part1 = (liftStateToPair a){accept = S.fromList []}
+               part2 = makeNBA (zip ps ls) ts [] as
+               trans12 = [((1, (qs, [])), l, (2, ([f], [f]))) | f <- S.toList (accept a)
+                                                   , (qs, l) <- combine (pres a f)]
+               in
+                insertTrans trans12 $ buchiUnion part1 part2
+
+
+-- | Make transition for pair of sets of states, as described in the article by Courcoubetis and Yannakakis
 pairTrans :: (Ord q, Ord a) => NBA q a l -> PowerPair q -> [(PowerPair q, a)]
 pairTrans a (qs1, qs2) = let ts1 = powerSucc a qs1
                              acc1 = [f | f <- qs1, S.member f (accept a)]
@@ -29,11 +49,14 @@ pairTrans a (qs1, qs2) = let ts1 = powerSucc a qs1
                               [((qs1', nub (qs2' l)), l) | (qs1', l) <- ts1]
 
 
+-- | Compute all transitions reachable from list of pairs of states
 pairTransClosure :: (Ord q, Ord a) => NBA q a l -> [PowerPair q] -> [(PowerPair q, a, PowerPair q)]
 pairTransClosure a ps = [(ps1, l, ps2) | (ps1, vs) <- M.assocs (genPowerSet a (M.fromList []) ps)
                                        , (ps2, l) <- vs]
 
 
+-- | Compute power-set construction for pairs of sets of states, as described in the article
+--   by Courcoubetis and Yannakakis
 genPowerSet ::  (Ord q, Ord a) =>
                 NBA q a l ->
                 M.Map (PowerPair q) [(PowerPair q, a)] ->
@@ -47,6 +70,7 @@ genPowerSet a m (p:ps) = let psl' = (pairTrans a p)
 genPowerSet a m [] = m
 
 
+-- | Lift states q and labels in an NBA to pair of states ([q], []) and pair of labels ([l], [])
 liftStateToPair :: (Ord q, Ord a) => NBA q a l -> NBA (PowerPair q) a ([l], [l])
 liftStateToPair a = let liftToPair q = ([q], []) in
   a{ states = S.map liftToPair (states a)
@@ -57,18 +81,9 @@ liftStateToPair a = let liftToPair q = ([q], []) in
    }
 
 
+-- | Return labels corresponding to list of states
 labels :: (Ord q) => [q] -> NBA q a l -> [l]
 labels qs a = [label q a | q <- qs]
 
 
-toLDBA :: (Ord q, Ord a) =>  NBA q a l -> NBA (Int, PowerPair q) a ([l], [l])
-toLDBA a = let ts = pairTransClosure a [([f],[f]) | f <- S.toList (accept a)]
-               ps = concat [[p1, p2] | (p1, a, p2) <- ts]
-               ls = [(labels q1 a, labels q2 a) | (q1, q2) <- ps]
-               as = [(qs1, qs2) | (qs1, qs2) <- ps, qs1 == qs2]
-               part2 = makeNBA (zip ps ls) ts [] as
-               trans12 = [((1, (qs, [])), l, (2, ([f], [f]))) | f <- S.toList (accept a)
-                                                   , (qs, l) <- combine (pres a f)]
-               part1 = (liftStateToPair a){accept = S.fromList []}
-               in
-                insertTrans trans12 $ buchiUnion part1 part2
+
